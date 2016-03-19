393f7e48a11748975c7e69de15dcc156eb4a78bd
leecmm <kbkyan@163.com>
2016-03-19 16:26:42 +0800

    添加刷新

--- TestApp/appView/projects/financing/financing.js
+++ TestApp/appView/projects/financing/financing.js
@@ -8,14 +8,17 @@ const {
   View,
   ScrollView,
   ListView,
-  Text  
+  Text,
+  RefreshControl,  
 } = React;
 
 class financing extends Component {
     constructor (props) {
     super(props)
+    this.pageIndex = 0
     this.state = {
       dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}), // 先初始化一个空的数据集合
+      loadMore: false,
       isRefreshing: false,
       isError: false
     }
@@ -38,13 +41,83 @@ fetchData() {
 }
 
 render() {
+  this.state.isError = false
   return (
     <ListView
       style={styles.container}
       dataSource={this.state.dataSource}
-      renderRow={this.renderRow} />
+      onEndReached={this._loadmore.bind(this)}
+      renderRow={this.renderRow.bind(this)}
+      onEndReachedThreshold = {29}
+      refreshControl={
+          <RefreshControl
+            refreshing={this.state.isRefreshing}
+            onRefresh={this._refresh.bind(this)}
+            tintColor='#aaaaaa'
+            title='Loading...'
+            progressBackgroundColor='#aaaaaa'/>
+      }/>
   );
 }
+
+  async _refresh () {
+    if (this.state.isRefreshing) {
+      return
+    }
+    this.setState({isRefreshing: true})
+
+    try {
+      this.dateArray = (await RequestUtils.getDateArray()).results
+      this.pageIndex = 0
+      let contentDataGroup = await RequestUtils.getContents(this.dateArray.slice(0, 10))
+      if (typeof contentDataGroup === 'undefined') {
+       return
+      }
+      console.log(contentDataGroup)
+      this.setState({
+        dataArray: contentDataGroup,
+        dataSource: this.state.dataSource.cloneWithRows(contentDataGroup),
+        isRefreshing: false
+      })
+    } catch (error) {
+      console.log(error)
+      this.setState({
+        isError: true,
+        isRefreshing: false
+      })
+    }
+
+    // 异步方法的数据只能在回调方法里获取。await可以让它顺序执行
+  }
+
+async _loadmore () {
+    if (this.state.loadMore) {
+      return
+    }
+
+    this.setState({loadMore: true})
+    console.log('===haha', this.state.loadMore)
+
+    try {
+      this.pageIndex += 10
+      let pageDate = this.dateArray.slice(this.pageIndex, this.pageIndex + 10)
+
+      let loadedContentGroup = await RequestUtils.getContents(pageDate)
+      let newContent = [...this.state.dataArray, ...loadedContentGroup] // put elements in loadedContentGroup into dataArray
+
+      this.setState({
+        dataArray: newContent,
+        dataSource: this.state.dataSource.cloneWithRows(newContent),
+        loadMore: false
+      })
+    } catch (error) {
+      console.log(error)
+      this.setState({
+        loadMore: false,
+        isError: true
+      })
+    }
+  }
 
 renderRow(contentData, sectionID, highlightRow){
 return(
